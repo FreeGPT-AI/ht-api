41e02d0075c84c3c6f6cc3a9acbc42283af40959
ThatLukinhasGuy <lucinhas@horsefucker.org>
2024-06-16 19:20:40 -0300

    Refactor code

--- api/responses/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pretty_json import PrettyJSONResponse
-from .chat_response import normal_chat_response, streaming_chat_response
-
-__all__ = [
-    "PrettyJSONResponse",
-    "normal_chat_response",
-    "streaming_chat_response"
-]
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-exec uvicorn api.main:app --host 0.0.0.0 --port 80 --workers 8 --loop uvloop &
-exec python3 -m bot.main
+exec uvicorn src.api:app --host 0.0.0.0 --port 80 --loop uvloop &
+exec python3 -m src.bot
